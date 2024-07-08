58767f655a6433140d2b22c37d4e1f953f1fc851
Julien Montagut <_@julienmontagut.com>
2024-07-08 17:44:22 +0200

    Reorganize and enhance dotfiles configuration

---
+++ config/nvim/lua/plugins-wip/cmp.lua
@@ -0,0 +1,7 @@
+return {
+    "hrsh7th/nvim-cmp",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-nvim-lsp-signature-help",
+    "hrsh7th/cmp-nvim-lsp-document-symbol",
+    "hrsh7th/cmp-nvim-lua",
+}
