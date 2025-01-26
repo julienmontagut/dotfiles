f3efb8ca1026b4d0e98c89fde6c798f6d097a334
Julien Montagut <_@julienmontagut.com>
2025-01-26 23:21:32 +0100

    Clean up unused configuration and environment files

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -116,9 +116,13 @@ require("oil").setup {
 }
 
 local builtin = require "telescope.builtin"
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+vim.keymap.set("n", "<leader>d", builtin.diagnostics, {})
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>j", builtin.jumplist, {})
+vim.keymap.set("n", "<leader>s", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>S", builtin.lsp_workspace_symbols, {})
 
 local lspconfig = require "lspconfig"
 lspconfig.astro.setup {}
