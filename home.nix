f2a80449bf2621069dfc01084201fe88534bccf0
Julien Montagut <_@julienmontagut.com>
2025-06-25 20:18:48 +0200

    Configure Zellij terminal multiplexer and layouts

--- home.nix
+++ home.nix
@@ -77,56 +77,101 @@ in {
       default_mode = "locked";
       mouse_mode = false;
       hide_session_name = true;
+      show_startup_tips = false;
       keybinds = {
         locked = { "bind \"Ctrl g\"" = { SwitchToMode = "Normal"; }; };
         normal = {
           "bind \"Ctrl g\"" = { SwitchToMode = "Locked"; };
-          "bind \"Ctrl p\"" = { SwitchToMode = "Pane"; };
-          "bind \"Ctrl t\"" = { SwitchToMode = "Tab"; };
-          "bind \"Ctrl r\"" = { SwitchToMode = "Resize"; };
-          "bind \"Ctrl s\"" = { SwitchToMode = "Scroll"; };
-          "bind \"Ctrl o\"" = { SwitchToMode = "Session"; };
-          "bind \"Ctrl h\"" = { SwitchToMode = "Move"; };
-          "bind \"Ctrl q\"" = "Quit";
+          "bind \"p\"" = { SwitchToMode = "Pane"; };
+          "bind \"t\"" = { SwitchToMode = "Tab"; };
+          "bind \"r\"" = { SwitchToMode = "Resize"; };
+          "bind \"s\"" = { SwitchToMode = "Scroll"; };
+          "bind \"o\"" = { SwitchToMode = "Session"; };
+          "bind \"h\"" = { SwitchToMode = "Move"; };
         };
         pane = {
           "bind \"Ctrl g\"" = { SwitchToMode = "Locked"; };
-          "bind \"Ctrl p\"" = { SwitchToMode = "Normal"; };
+          "bind \"Ctrl c\"" = { SwitchToMode = "Normal"; };
           "bind \"h\" \"Left\"" = { MoveFocus = "Left"; };
           "bind \"l\" \"Right\"" = { MoveFocus = "Right"; };
           "bind \"j\" \"Down\"" = { MoveFocus = "Down"; };
           "bind \"k\" \"Up\"" = { MoveFocus = "Up"; };
-          "bind \"p\"" = "SwitchFocus";
-          "bind \"n\"" = [ "NewPane" { SwitchToMode = "Normal"; } ];
-          "bind \"d\"" = [ { NewPane = "Down"; } { SwitchToMode = "Normal"; } ];
-          "bind \"r\"" =
-            [ { NewPane = "Right"; } { SwitchToMode = "Normal"; } ];
-          "bind \"x\"" = [ "CloseFocus" { SwitchToMode = "Normal"; } ];
-          "bind \"f\"" =
-            [ "ToggleFocusFullscreen" { SwitchToMode = "Normal"; } ];
-          "bind \"z\"" = [ "TogglePaneFrames" { SwitchToMode = "Normal"; } ];
-          "bind \"w\"" = [ "ToggleFloatingPanes" { SwitchToMode = "Normal"; } ];
-          "bind \"e\"" =
-            [ "TogglePaneEmbedOrFloating" { SwitchToMode = "Normal"; } ];
+          "bind \"p\"" = { SwitchFocus = { }; };
+          "bind \"n\"" = {
+            NewPane = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"d\"" = {
+            NewPane = "Down";
+            SwitchToMode = "Normal";
+          };
+          "bind \"r\"" = {
+            NewPane = "Right";
+            SwitchToMode = "Normal";
+          };
+          "bind \"x\"" = {
+            CloseFocus = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"f\"" = {
+            ToggleFocusFullscreen = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"z\"" = {
+            TogglePaneFrames = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"w\"" = {
+            ToggleFloatingPanes = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"e\"" = {
+            TogglePaneEmbedOrFloating = { };
+            SwitchToMode = "Normal";
+          };
         };
         tab = {
           "bind \"Ctrl g\"" = { SwitchToMode = "Locked"; };
-          "bind \"Ctrl t\"" = { SwitchToMode = "Normal"; };
-          "bind \"h\" \"Left\" \"Up\" \"k\"" = "GoToPreviousTab";
-          "bind \"l\" \"Right\" \"Down\" \"j\"" = "GoToNextTab";
-          "bind \"n\"" = [ "NewTab" { SwitchToMode = "Normal"; } ];
-          "bind \"x\"" = [ "CloseTab" { SwitchToMode = "Normal"; } ];
-          "bind \"s\"" = [ "ToggleActiveSyncTab" { SwitchToMode = "Normal"; } ];
-          "bind \"1\"" = [ { GoToTab = 1; } { SwitchToMode = "Normal"; } ];
-          "bind \"2\"" = [ { GoToTab = 2; } { SwitchToMode = "Normal"; } ];
-          "bind \"3\"" = [ { GoToTab = 3; } { SwitchToMode = "Normal"; } ];
-          "bind \"4\"" = [ { GoToTab = 4; } { SwitchToMode = "Normal"; } ];
-          "bind \"5\"" = [ { GoToTab = 5; } { SwitchToMode = "Normal"; } ];
-          "bind \"Tab\"" = "ToggleTab";
+          "bind \"Ctrl c\"" = { SwitchToMode = "Normal"; };
+          "bind \"h\" \"Left\" \"Up\" \"k\"" = { GoToPreviousTab = { }; };
+          "bind \"l\" \"Right\" \"Down\" \"j\"" = { GoToNextTab = { }; };
+          "bind \"n\"" = {
+            NewTab = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"x\"" = {
+            CloseTab = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"s\"" = {
+            ToggleActiveSyncTab = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"1\"" = {
+            GoToTab = 1;
+            SwitchToMode = "Normal";
+          };
+          "bind \"2\"" = {
+            GoToTab = 2;
+            SwitchToMode = "Normal";
+          };
+          "bind \"3\"" = {
+            GoToTab = 3;
+            SwitchToMode = "Normal";
+          };
+          "bind \"4\"" = {
+            GoToTab = 4;
+            SwitchToMode = "Normal";
+          };
+          "bind \"5\"" = {
+            GoToTab = 5;
+            SwitchToMode = "Normal";
+          };
+          "bind \"Tab\"" = { ToggleTab = { }; };
         };
         resize = {
           "bind \"Ctrl g\"" = { SwitchToMode = "Locked"; };
-          "bind \"Ctrl r\"" = { SwitchToMode = "Normal"; };
+          "bind \"Ctrl c\"" = { SwitchToMode = "Normal"; };
           "bind \"h\" \"Left\"" = { Resize = "Increase Left"; };
           "bind \"j\" \"Down\"" = { Resize = "Increase Down"; };
           "bind \"k\" \"Up\"" = { Resize = "Increase Up"; };
@@ -140,19 +185,24 @@ in {
         };
         scroll = {
           "bind \"Ctrl g\"" = { SwitchToMode = "Locked"; };
-          "bind \"Ctrl s\"" = { SwitchToMode = "Normal"; };
-          "bind \"Ctrl c\"" = [ "ScrollToBottom" { SwitchToMode = "Normal"; } ];
-          "bind \"j\" \"Down\"" = "ScrollDown";
-          "bind \"k\" \"Up\"" = "ScrollUp";
-          "bind \"Ctrl f\" \"PageDown\" \"Right\" \"l\"" = "PageScrollDown";
-          "bind \"Ctrl b\" \"PageUp\" \"Left\" \"h\"" = "PageScrollUp";
-          "bind \"d\"" = "HalfPageScrollDown";
-          "bind \"u\"" = "HalfPageScrollUp";
+          "bind \"Ctrl c\"" = { SwitchToMode = "Normal"; };
+          "bind \"Ctrl b\"" = {
+            ScrollToBottom = { };
+            SwitchToMode = "Normal";
+          };
+          "bind \"j\" \"Down\"" = { ScrollDown = { }; };
+          "bind \"k\" \"Up\"" = { ScrollUp = { }; };
+          "bind \"Ctrl f\" \"PageDown\" \"Right\" \"l\"" = {
+            PageScrollDown = { };
+          };
+          "bind \"Ctrl b\" \"PageUp\" \"Left\" \"h\"" = { PageScrollUp = { }; };
+          "bind \"d\"" = { HalfPageScrollDown = { }; };
+          "bind \"u\"" = { HalfPageScrollUp = { }; };
         };
         session = {
           "bind \"Ctrl g\"" = { SwitchToMode = "Locked"; };
-          "bind \"Ctrl o\"" = { SwitchToMode = "Normal"; };
-          "bind \"d\"" = "Detach";
+          "bind \"Ctrl c\"" = { SwitchToMode = "Normal"; };
+          "bind \"d\"" = { Detach = { }; };
         };
       };
     };
