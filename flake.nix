07ec3f86c559e161c9b1bbfb3d936ba6109bb5fd
Julien Montagut <_@julienmontagut.com>
2025-11-20 17:36:15 +0100

    Start migration to standalone home-manager

--- flake.nix
+++ flake.nix
@@ -1,158 +1,21 @@
 {
-  description = "Julien's macOS flake";
+  description = "My Home Manager Flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-25.05";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur = {
-      url = "github:nix-community/NUR";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    catppuccin.url = "github:catppuccin/nix";
   };
 
-  outputs =
-    inputs@{ self, nixpkgs, nix-darwin, home-manager, nur, catppuccin, ... }:
-    let
-      configuration = { pkgs, ... }: {
-
-        nix.settings = {
-          experimental-features = "nix-command flakes";
-          trusted-users = [ "root" "@admin" "@wheel" ];
-        };
-
-        nix.gc.automatic = true;
-        nix.optimise.automatic = true;
-
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = "aarch64-darwin";
-
-        # Allow non-free packages
-        nixpkgs.config = {
-          allowUnfree = true;
-          allowUnfreePredicate = _: true;
-        };
-
-        nixpkgs.overlays = [ nur.overlays.default ];
-
-        # Packages installed in system profile
-        environment.systemPackages = with pkgs; [
-          lua
-          luajit
-          podman
-
-          # Packages to install on macos
-          defaultbrowser
-        ];
-
-        services.netbird.enable = true;
-
-        # Variables set in system profile
-        environment.variables = {
-          HOMEBREW_NO_ANALYTICS = "1";
-          HOMEBREW_NO_ENV_HINTS = "1";
-        };
-
-        # Add ability to used TouchID for sudo authentication
-        security.pam.services.sudo_local.touchIdAuth = true;
-
-        # System configuration
-        system = {
-
-          primaryUser = "julien";
-
-          defaults = {
-            menuExtraClock.Show24Hour = true;
-
-            # Trackpad defaults enabling tap to click, etc.
-            trackpad = {
-              Clicking = true;
-              TrackpadRightClick = true;
-              TrackpadThreeFingerDrag = true;
-            };
-          };
-
-          keyboard = {
-            enableKeyMapping = true;
-            remapCapsLockToControl = true;
-            nonUS.remapTilde = true;
-          };
-
-          # Set Git commit hash for darwin-version.
-          configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          stateVersion = 5;
-        };
-
-        # Enable ZSH shell support in nix-darwin.
-        # programs.zsh.enable = true;
-
-        homebrew = {
-          enable = true;
-          onActivation = {
-            autoUpdate = true;
-            upgrade = true;
-            cleanup = "zap";
-          };
-
-          casks = [
-            "anytype"
-            "appcleaner"
-            "bitwarden"
-            "claude"
-            "element"
-            "firefox"
-            "font-inter"
-            "font-lilex"
-            "font-lilex-nerd-font"
-            "font-literata"
-            "ghostty"
-            "google-chrome"
-            "google-drive"
-            "jetbrains-toolbox"
-            "motion"
-            "orbstack"
-            "spotify"
-            "steam"
-            "utm"
-            "visual-studio-code"
-            "whatsapp"
-            "zed"
-          ];
-        };
-
-        users.users.julien = {
-          name = "julien";
-          home = "/Users/julien";
-        };
-      };
-    in {
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#MacBook-de-Julien
-      darwinConfigurations.Macbook = nix-darwin.lib.darwinSystem {
-        modules = [
-          configuration
-
-          #home manager
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.extraSpecialArgs = { inherit inputs nur; };
-            home-manager.backupFileExtension = "previous";
-            home-manager.users.julien.imports =
-              [ ./home.nix catppuccin.homeModules.catppuccin ];
-          }
-        ];
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations = {
+      "julien" = home-manager.lib.homeManagerConfiguration {
+        # System is very important!
+        pkgs = import nixpkgs { system = "aarch64-darwin"; };
+        modules = [ ./home.nix ];
       };
     };
+  };
 }
