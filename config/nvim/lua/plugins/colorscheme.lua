4469190250a5133a1c77f284d2b9179d57de811f
Julien Montagut <_@julienmontagut.com>
2025-11-29 00:27:13 +0100

    Switch to tokyonight colorscheme and update nixpkgs

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,23 @@
 return {
     {
-        "EdenEast/nightfox.nvim",
-        opts = {},
-        config = function(_, opts)
-            require("nightfox").setup({ options = opts })
-            vim.cmd("colorscheme carbonfox")
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("tokyonight").setup({
+                style = "night",
+                transparent = false,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = true },
+                    keywords = { italic = true },
+                    functions = {},
+                    variables = {},
+                    sidebars = "dark",
+                    floats = "dark",
+                },
+            })
+            vim.cmd([[colorscheme tokyonight]])
         end,
     },
 }
--- flake.nix
+++ flake.nix
@@ -1,23 +1,20 @@
 {
-  description = "My Home Manager Flake";
+  description = "Julien's Home Configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    catppuccin.url = "github:catppuccin/nix";
+   nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { nixpkgs, home-manager, catppuccin, ... }: {
+  outputs = { nixpkgs, home-manager, ... }: {
     homeConfigurations = {
       "julien" = home-manager.lib.homeManagerConfiguration {
-        # System is very important!
         pkgs = import nixpkgs { system = "aarch64-darwin"; };
         modules = [ 
           ./home.nix
-          catppuccin.homeModules.catppuccin
         ];
       };
     };
