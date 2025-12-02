1c760302c5cc54836589ec655d72b49715e7394b
Julien Montagut <_@julienmontagut.com>
2025-12-02 13:02:29 +0100

    Add karabiner and sketchybar top bar configuration

--- config/sketchybar/plugins/front_app.sh
+++ config/sketchybar/plugins/front_app.sh
@@ -3,9 +3,9 @@
 # Get the front app name
 FRONT_APP=$(aerospace list-windows --focused --format "%{app-name}")
 
-# Update the bar
+# Update the bar - hide if no focused app
 if [ -n "$FRONT_APP" ]; then
-  sketchybar --set front_app label="$FRONT_APP"
+  sketchybar --set front_app label="$FRONT_APP" drawing=on
 else
-  sketchybar --set front_app label="Desktop"
+  sketchybar --set front_app drawing=off
 fi
