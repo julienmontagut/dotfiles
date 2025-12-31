0bb9617a390d036f619cd845852fc0c395659ef4
Julien Montagut <_@julienmontagut.com>
2025-12-31 17:31:33 +0100

    Restructure platform configuration and script layout

--- flake.nix
+++ flake.nix
@@ -7,31 +7,39 @@
       url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    stylix = {
+      url = "github:nix-community/stylix/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, home-manager, ... }:
-    {
-      homeConfigurations = {
-        "julien@mac" = home-manager.lib.homeManagerConfiguration {
+    { nixpkgs, home-manager, stylix, ... }:
+    let
+      mkHomeConfig = { system, modules }:
+        home-manager.lib.homeManagerConfiguration {
           pkgs = import nixpkgs {
-            system = "aarch64-darwin";
+            inherit system;
             config.allowUnfree = true;
           };
+          modules = [ stylix.homeModules.stylix ] ++ modules;
+        };
+    in
+    {
+      homeConfigurations = {
+        "macos" = mkHomeConfig {
+          system = "aarch64-darwin";
           modules = [
             ./home.nix
-            ./hosts/macos.nix
+            ./platforms/macos.nix
           ];
         };
 
-        "julien@linux" = home-manager.lib.homeManagerConfiguration {
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-            config.allowUnfree = true;
-          };
-          modules = [ 
-            ./home.nix 
-            ./hosts/linux.nix
+        "linux" = mkHomeConfig {
+          system = "x86_64-linux";
+          modules = [
+            ./home.nix
+            ./platforms/linux.nix
           ];
         };
       };
