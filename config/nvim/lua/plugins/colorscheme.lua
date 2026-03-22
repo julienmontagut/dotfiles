8de2fe6f29e57e5618e9ebcf15c7a233ec3eb3da
Julien Montagut <_@julienmontagut.com>
2026-03-22 23:29:26 +0100

    Add experimental basalt neovim color scheme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,23 +1,15 @@
 return {
     {
         "folke/tokyonight.nvim",
+        lazy = true,
+    },
+    {
+        dir = vim.fn.stdpath("config") .. "/colors",
+        name = "basalt",
         lazy = false,
         priority = 1000,
         config = function()
-            require("tokyonight").setup({
-                style = "storm",
-                transparent = true,
-                terminal_colors = true,
-                styles = {
-                    comments = { italic = true },
-                    keywords = { italic = true },
-                    functions = {},
-                    variables = {},
-                    sidebars = "transparent",
-                    floats = "transparent",
-                },
-            })
-            vim.cmd([[colorscheme tokyonight]])
+            vim.cmd([[colorscheme basalt]])
         end,
     },
 }
