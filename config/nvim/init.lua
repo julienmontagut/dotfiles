9a7d5e879f8bddb350e7d1ba3c6ba6379e4691de
Julien Montagut <_@julienmontagut.com>
2026-04-07 17:51:41 +0200

    Add alacritty, neovim, and tmux configuration

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,69 +1,204 @@
--- Set the leader key to space
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Basic editor settings
+-- General user interface
 vim.opt.number = true
+vim.opt.colorcolumn = "80,100"
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+
+-- Tabs and indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+-- Search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
+
+-- Behaviour
+vim.opt.clipboard = "unnamedplus"
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.swapfile = false
 vim.opt.undofile = true
 
--- Exit insert mode with jk
-vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
-
-vim.opt.colorcolumn = "80"
-vim.opt.cursorline = true
-vim.opt.termguicolors = true
-vim.opt.signcolumn = "yes"
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Disable unused providers
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
--- vim.g.python3_host_prog = vim.fn.exepath("python3")
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--branch=stable",
-        lazyrepo,
-        lazypath,
-    })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
+vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
+
+-- Plugins
+vim.pack.add {
+    "https://github.com/folke/flash.nvim",
+    "https://github.com/folke/snacks.nvim",
+    "https://github.com/folke/trouble.nvim",
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/nvim-mini/mini.icons",
+    { src = "https://github.com/saghen/blink.cmp", version = "v1" },
+    "https://github.com/stevearc/oil.nvim",
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+    "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
+    "https://github.com/seblj/roslyn.nvim",
+}
+
+-- Plugin setup
+require("blink.cmp").setup{}
+require("flash").setup{}
+require("mini.icons").setup{}
+require("trouble").setup{}
+
+-- Oil
+require("oil").setup {
+    default_file_explorer = true,
+    show_hidden = true,
+}
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Snacks
+require("snacks").setup {
+    picker = { enabled = true },
+    indent = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+}
+
+vim.keymap.set("n", "<leader>e", function() Snacks.picker.files() end, { desc = "Find files" })
+vim.keymap.set("n", "<leader>sf", function() Snacks.picker.files() end, { desc = "Find files" })
+vim.keymap.set("n", "<leader>sg", function() Snacks.picker.grep() end, { desc = "Grep" })
+vim.keymap.set("n", "<leader>sb", function() Snacks.picker.buffers() end, { desc = "Buffers" })
+vim.keymap.set("n", "<leader>sh", function() Snacks.picker.help() end, { desc = "Help pages" })
+vim.keymap.set("n", "<leader>sr", function() Snacks.picker.recent() end, { desc = "Recent files" })
+vim.keymap.set("n", "<leader>sd", function() Snacks.picker.diagnostics() end, { desc = "Diagnostics" })
+vim.keymap.set("n", "<leader>ss", function() Snacks.picker.lsp_symbols() end, { desc = "LSP symbols" })
+vim.keymap.set("n", "<leader>sw", function() Snacks.picker.grep_word() end, { desc = "Grep word" })
+vim.keymap.set("n", "<leader>sc", function() Snacks.picker.commands() end, { desc = "Commands" })
+vim.keymap.set("n", "<leader>sk", function() Snacks.picker.keymaps() end, { desc = "Keymaps" })
+vim.keymap.set("n", "<leader>s/", function() Snacks.picker.grep_buffers() end, { desc = "Grep open buffers" })
+
+-- Treesitter: highlighting is native in neovim 0.12.
+-- Bundled parsers: c, lua, markdown, vim, vimdoc.
+-- Install others with :TSInstall (requires tree-sitter CLI).
+local ts_parsers = {
+    "c_sharp", "rust", "lua", "bash",
+    "swift", "json", "toml", "nickel",
+    "html", "css",
+}
+local ts_installed = require("nvim-treesitter.config").get_installed()
+local ts_to_install = vim.iter(ts_parsers)
+    :filter(function(p) return not vim.tbl_contains(ts_installed, p) end)
+    :totable()
+if #ts_to_install > 0 then
+    require("nvim-treesitter").install(ts_to_install)
 end
-vim.opt.rtp:prepend(lazypath)
 
--- Load all plugins
-require("lazy").setup({
-    spec = {
-        { import = "plugins" },
-        { import = "plugins.lang" },
-        performance = {
-            rtp = {
-                disabled_plugins = {
-                    "gzip",
-                    "matchit",
-                    "matchparen",
-                    "netrwPlugin",
-                    "tarPlugin",
-                    "tohtml",
-                    "tutor",
-                    "zipPlugin",
-                },
-            },
+-- Treesitter textobjects
+require("nvim-treesitter-textobjects").setup {
+    select = { lookahead = true },
+    move = { set_jumps = true },
+}
+
+local ts_select = require("nvim-treesitter-textobjects.select")
+local ts_move = require("nvim-treesitter-textobjects.move")
+local ts_swap = require("nvim-treesitter-textobjects.swap")
+
+-- Select
+for key, query in pairs({
+    ["af"] = "@function.outer", ["if"] = "@function.inner",
+    ["ac"] = "@class.outer",    ["ic"] = "@class.inner",
+    ["aa"] = "@parameter.outer", ["ia"] = "@parameter.inner",
+    ["ab"] = "@block.outer",    ["ib"] = "@block.inner",
+    ["al"] = "@loop.outer",     ["il"] = "@loop.inner",
+}) do
+    vim.keymap.set({ "x", "o" }, key, function()
+        ts_select.select_textobject(query, "textobjects")
+    end)
+end
+
+-- Move (next/prev start and end)
+for key, query in pairs({
+    ["]f"] = "@function.outer", ["]c"] = "@class.outer",
+    ["]a"] = "@parameter.inner", ["]b"] = "@block.outer",
+}) do
+    vim.keymap.set({ "n", "x", "o" }, key, function()
+        ts_move.goto_next_start(query, "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, key:upper(), function()
+        ts_move.goto_next_end(query, "textobjects")
+    end)
+    local prev_key = key:gsub("%]", "[")
+    vim.keymap.set({ "n", "x", "o" }, prev_key, function()
+        ts_move.goto_previous_start(query, "textobjects")
+    end)
+    vim.keymap.set({ "n", "x", "o" }, prev_key:upper(), function()
+        ts_move.goto_previous_end(query, "textobjects")
+    end)
+end
+
+-- Swap
+vim.keymap.set("n", "<leader>]a", function() ts_swap.swap_next("@parameter.inner") end, { desc = "Swap next parameter" })
+vim.keymap.set("n", "<leader>[a", function() ts_swap.swap_previous("@parameter.inner") end, { desc = "Swap prev parameter" })
+
+-- LSP Configuration
+vim.lsp.config("lua_ls", {
+    settings = {
+        Lua = {
+            diagnostics = { globals = { "vim" } },
+            workspace = { checkThirdParty = false },
+            completion = { callSnippet = "Replace" },
         },
     },
-    -- lockfile = vim.fn.stdpath("data") .. "/lazy-lock.json",
+})
+
+vim.lsp.config("rust_analyzer", {
+    settings = {
+        ["rust-analyzer"] = {
+            checkOnSave = { command = "clippy" },
+            cargo = { allFeatures = true },
+            procMacro = { enable = true },
+        },
+    },
+})
+
+vim.lsp.enable({
+    "lua_ls",
+    "rust_analyzer",
+    "taplo",
+    "bashls",
+    "jsonls",
+    "html",
+    "cssls",
+    "sourcekit",
+    "nickel_ls",
+})
+
+-- Roslyn (C#) - uses roslyn.nvim for the latest Roslyn LSP
+require("roslyn").setup {
+    filewatching = true,
+}
+
+-- Project root detection (best match first, git fallback)
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function(args)
+        -- Skip non-file buffers (including oil.nvim)
+        if vim.bo[args.buf].buftype ~= "" then return end
+
+        local root = vim.fs.root(args.buf, function(name)
+            return name:match("%.sln$") ~= nil
+        end)
+        or vim.fs.root(args.buf, { "Cargo.toml" })
+        or vim.fs.root(args.buf, { "package.json" })
+        or vim.fs.root(args.buf, { "go.mod" })
+        or vim.fs.root(args.buf, { "pyproject.toml", "setup.py" })
+        or vim.fs.root(args.buf, { ".git" })
+
+        if root then
+            vim.fn.chdir(root)
+        end
+    end,
 })
